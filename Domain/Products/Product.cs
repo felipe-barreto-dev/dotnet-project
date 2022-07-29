05e38bb17d47d6b8b458b806db79c4f7592de398
felipe-barreto-dev <fbarreto.dev@gmail.com>
2022-07-29 10:49:58 -0300

    feat:  first steps

---
+++ Domain/Products/Product.cs
@@ -0,0 +1,10 @@
+namespace dotnet_project.Domain.Products;
+
+public class Product : Entity {
+    public string Name { get; set; }
+    public bool HasStock { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
+    public string Description { get; set; }
+}
+ 
