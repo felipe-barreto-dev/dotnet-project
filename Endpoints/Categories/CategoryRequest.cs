63f4bfbbed9f85df3e5c925edf59ba31c4c158c2
felipe-barreto-dev <fbarreto.dev@gmail.com>
2022-12-25 21:26:16 -0300

    remove old code

--- Endpoints/Categories/CategoryRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnet_project.Endpoints.Categories
-{
-    public class CategoryRequest
-    {
-        public string Name { get; set; }
-        public bool Active { get; set; }
-    }
-}
